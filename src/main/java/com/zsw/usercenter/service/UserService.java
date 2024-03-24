73ba52d75fbd0c0f4724948e3cfd2c0306d1e43c
rein153 <2499530937@qq.com>
2024-03-24 23:09:01 +0800

    login

--- src/main/java/com/zsw/usercenter/service/UserService.java
+++ src/main/java/com/zsw/usercenter/service/UserService.java
@@ -5,13 +5,21 @@ import com.baomidou.mybatisplus.extension.service.IService;
 
 /**
 * @author zsw
-* @description 针对表【user(用户)】的数据库操作Service
-* @createDate 2024-03-11 23:30:35
+*
 */
 public interface UserService extends IService<User> {
 
     /**
-     * 用户登录
+     * 用户注册
+     * @param userAccount 用户账户
+     * @param userPassword 用户密码
+     * @param checkPassword 校验密码
+     * @return 新用户 id
      */
-    long UserRegist()
+    long userRegister(String userAccount, String userPassword, String checkPassword);
+
+    /**
+     *
+     */
+
 }
--- src/main/java/com/zsw/usercenter/service/impl/UserServiceImpl.java
+++ src/main/java/com/zsw/usercenter/service/impl/UserServiceImpl.java
@@ -1,20 +1,60 @@
 package com.zsw.usercenter.service.impl;
 
+import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
 import com.zsw.usercenter.model.domain.User;
 import com.zsw.usercenter.service.UserService;
 import com.zsw.usercenter.mapper.UserMapper;
+import org.apache.commons.lang3.StringUtils;
 import org.springframework.stereotype.Service;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 /**
 * @author zsw
-* @description 针对表【user(用户)】的数据库操作Service实现
-* @createDate 2024-03-11 23:30:35
+* 用户服务实现类
 */
 @Service
 public class UserServiceImpl extends ServiceImpl<UserMapper, User>
     implements UserService{
 
+    @Override
+    public long userRegister(String userAccount, String userPassword, String checkPassword) {
+        // 1.校验
+        if(StringUtils.isAllBlank(userAccount,userPassword,checkPassword)){
+            return -1;
+        }
+        if(userAccount.length() < 4){
+            return -1;
+        }
+        if(userPassword.length() < 8 || checkPassword.length() < 8){
+            return -1;
+        }
+
+        // 账户不能包含特殊字符
+        String validPattern = "[`~!@#$%^&*()+=|{}':;',\\\\[\\\\].<>/?~！@#￥%……&*（）——+|{}【】‘；：”“’。，、？]";
+        Matcher matcher = Pattern.compile(validPattern).matcher(userAccount);
+        if(matcher.find()){
+            return -1;
+        }
+        // 密码和校验密码
+        if(!userPassword.equals(checkPassword)){
+            return -1;
+        }
+
+        // 尽量先不查就不查
+        // 账户不能重复
+        QueryWrapper<User> queryWrapper = new QueryWrapper<>();
+        queryWrapper.eq("userAccount",userAccount);
+        long count = this.count(queryWrapper);
+        if(count > 0){
+            return -1;
+        }
+
+
+        return 0;
+    }
 }
 
 
