0592a9f0f212129fee4a2c59750d05fcd780cfb2
rein153 <2499530937@qq.com>
2024-03-24 23:50:07 +0800

    test

---
+++ src/test/java/com/zsw/usercenter/service/UserServiceTest.java
@@ -0,0 +1,22 @@
+package com.zsw.usercenter.service;
+
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+/**
+ * @author zsw
+ */
+@SpringBootTest
+public class UserServiceTest {
+
+    @Test
+    public void testAddUser(){
+
+    }
+
+    @Test
+    public void userRegister() {
+    }
+}
